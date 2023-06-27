953ef7fd854f430487732c775b7d65815d2ef80a
Gabriel Sanches <gabriel@gsr.dev>
2023-06-27 19:26:29 -0300

    ejdtls: Adapt handlers

--- src/language_features/eclipse_jdt_ls.rs
+++ src/language_features/eclipse_jdt_ls.rs
@@ -6,21 +6,34 @@ use lsp_types::*;
 
 pub fn organize_imports(meta: EditorMeta, ctx: &mut Context) {
     let file_uri = Url::from_file_path(&meta.buffile).unwrap();
-
     let file_uri: String = file_uri.into();
-    let req_params = ExecuteCommandParams {
-        command: "java.edit.organizeImports".to_string(),
-        arguments: vec![serde_json::json!(file_uri)],
-        ..ExecuteCommandParams::default()
-    };
+
+    let req_params = ctx
+        .language_servers
+        .keys()
+        .map(|server_name| {
+            (
+                server_name.clone(),
+                vec![ExecuteCommandParams {
+                    command: "java.edit.organizeImports".to_string(),
+                    arguments: vec![serde_json::json!(file_uri)],
+                    ..ExecuteCommandParams::default()
+                }],
+            )
+        })
+        .collect();
+
     ctx.call::<ExecuteCommand, _>(
         meta,
-        RequestParams::All(vec![req_params]),
-        move |ctx: &mut Context, meta, mut response| {
-            if let Some((_, response)) = response.pop() {
-                if let Some(response) = response {
-                    organize_imports_response(meta, serde_json::from_value(response).unwrap(), ctx);
-                }
+        RequestParams::Each(req_params),
+        move |ctx, meta, results| {
+            let response = match results.into_iter().find(|(_, v)| v.is_some()) {
+                Some((_, result)) => result,
+                None => None,
+            };
+
+            if let Some(response) = response {
+                organize_imports_response(meta, serde_json::from_value(response).unwrap(), ctx);
             }
         },
     );
