a49453d9495dbe1e690ed9495ac070120755d31f
Gabriel Sanches <gabriel@gsr.dev>
2023-06-27 19:28:57 -0300

    inlay_hints: Handle multiple responses

--- src/language_features/inlay_hints.rs
+++ src/language_features/inlay_hints.rs
@@ -11,7 +11,7 @@ use crate::{
     context::{Context, RequestParams},
     markup::escape_kakoune_markup,
     position::lsp_position_to_kakoune,
-    types::{EditorMeta, EditorParams},
+    types::{EditorMeta, EditorParams, ServerName},
     util::{editor_quote, escape_tuple_element},
 };
 
@@ -21,46 +21,74 @@ struct InlayHintsOptions {
 }
 
 pub fn inlay_hints(meta: EditorMeta, params: EditorParams, ctx: &mut Context) {
-    let entry = ctx.language_servers.first_key_value().unwrap();
-    if meta.fifo.is_none() && !attempt_server_capability(entry, &meta, CAPABILITY_INLAY_HINTS) {
+    let eligible_servers: Vec<_> = ctx
+        .language_servers
+        .iter()
+        .filter(|srv| attempt_server_capability(*srv, &meta, CAPABILITY_INLAY_HINTS))
+        .collect();
+    if meta.fifo.is_none() && eligible_servers.is_empty() {
         return;
     }
 
     let params = InlayHintsOptions::deserialize(params).unwrap();
-    let req_params = InlayHintParams {
-        work_done_progress_params: Default::default(),
-        text_document: TextDocumentIdentifier {
-            uri: Url::from_file_path(&meta.buffile).unwrap(),
-        },
-        range: Range::new(Position::new(0, 0), Position::new(params.buf_line_count, 0)),
-    };
+    let req_params = eligible_servers
+        .into_iter()
+        .map(|(server_name, _)| {
+            (
+                server_name.clone(),
+                vec![InlayHintParams {
+                    work_done_progress_params: Default::default(),
+                    text_document: TextDocumentIdentifier {
+                        uri: Url::from_file_path(&meta.buffile).unwrap(),
+                    },
+                    range: Range::new(Position::new(0, 0), Position::new(params.buf_line_count, 0)),
+                }],
+            )
+        })
+        .collect();
     ctx.call::<InlayHintRequest, _>(
         meta,
-        RequestParams::All(vec![req_params]),
-        move |ctx, meta, mut response| {
-            if let Some((_, response)) = response.pop() {
-                inlay_hints_response(meta, response.unwrap_or_default(), ctx)
-            }
+        RequestParams::Each(req_params),
+        move |ctx, meta, results| {
+            let results = results
+                .into_iter()
+                .flat_map(|(server_name, v)| {
+                    let v: Vec<_> = v
+                        .unwrap_or_default()
+                        .into_iter()
+                        .map(|v| (server_name.clone(), v))
+                        .collect();
+                    v
+                })
+                .collect();
+            inlay_hints_response(meta, results, ctx)
         },
     );
 }
 
-pub fn inlay_hints_response(meta: EditorMeta, inlay_hints: Vec<InlayHint>, ctx: &mut Context) {
+pub fn inlay_hints_response(
+    meta: EditorMeta,
+    inlay_hints: Vec<(ServerName, InlayHint)>,
+    ctx: &mut Context,
+) {
     let document = match ctx.documents.get(&meta.buffile) {
         Some(document) => document,
         None => return,
     };
-    let (_, server) = ctx.language_servers.first_key_value().unwrap();
     let ranges = inlay_hints
         .into_iter()
         .map(
-            |InlayHint {
-                 position,
-                 label,
-                 padding_left,
-                 padding_right,
-                 ..
-             }| {
+            |(
+                server_name,
+                InlayHint {
+                    position,
+                    label,
+                    padding_left,
+                    padding_right,
+                    ..
+                },
+            )| {
+                let server = &ctx.language_servers[&server_name];
                 let position =
                     lsp_position_to_kakoune(&position, &document.text, server.offset_encoding);
                 let label = match label {
