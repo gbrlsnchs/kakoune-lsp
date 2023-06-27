395bfaa1d05c945a51f20ae39ab1e76893bf7197
Gabriel Sanches <gabriel@gsr.dev>
2023-06-27 19:29:08 -0300

    rename: Handle multiple responses

--- src/language_features/rename.rs
+++ src/language_features/rename.rs
@@ -10,34 +10,55 @@ use url::Url;
 use super::super::workspace;
 
 pub fn text_document_rename(meta: EditorMeta, params: EditorParams, ctx: &mut Context) {
-    let (_, server) = ctx.language_servers.first_key_value().unwrap();
     let params = TextDocumentRenameParams::deserialize(params).unwrap();
-    let req_params = RenameParams {
-        text_document_position: TextDocumentPositionParams {
-            text_document: TextDocumentIdentifier {
-                uri: Url::from_file_path(&meta.buffile).unwrap(),
-            },
-            position: get_lsp_position(server, &meta.buffile, &params.position, ctx).unwrap(),
-        },
-        new_name: params.new_name,
-        work_done_progress_params: Default::default(),
-    };
+    let req_params = ctx
+        .language_servers
+        .iter()
+        .map(|(server_name, server_settings)| {
+            (
+                server_name.clone(),
+                vec![RenameParams {
+                    text_document_position: TextDocumentPositionParams {
+                        text_document: TextDocumentIdentifier {
+                            uri: Url::from_file_path(&meta.buffile).unwrap(),
+                        },
+                        position: get_lsp_position(
+                            server_settings,
+                            &meta.buffile,
+                            &params.position,
+                            ctx,
+                        )
+                        .unwrap(),
+                    },
+                    new_name: params.new_name.clone(),
+                    work_done_progress_params: Default::default(),
+                }],
+            )
+        })
+        .collect();
     ctx.call::<Rename, _>(
         meta,
-        RequestParams::All(vec![req_params]),
-        move |ctx: &mut Context, meta, mut result| {
-            if let Some((_, result)) = result.pop() {
-                editor_rename(meta, result, ctx)
-            }
+        RequestParams::Each(req_params),
+        move |ctx: &mut Context, meta, results| {
+            let result = match results.into_iter().find(|(_, v)| v.is_some()) {
+                Some(result) => result,
+                None => {
+                    let entry = ctx.language_servers.first_entry().unwrap();
+                    (entry.key().clone(), None)
+                }
+            };
+
+            editor_rename(meta, result, ctx)
         },
     );
 }
 
 // TODO handle version, so change is not applied if buffer is modified (and need to show a warning)
-fn editor_rename(meta: EditorMeta, result: Option<WorkspaceEdit>, ctx: &mut Context) {
+fn editor_rename(meta: EditorMeta, result: (ServerName, Option<WorkspaceEdit>), ctx: &mut Context) {
+    let (server_name, result) = result;
     if result.is_none() {
         return;
     }
     let result = result.unwrap();
-    workspace::apply_edit(meta, result, ctx);
+    workspace::apply_edit(&server_name, meta, result, ctx);
 }
