79dd3adc15d3a15213eb64188311d97127bd5be5
Gabriel Sanches <gabriel@gsr.dev>
2023-06-27 19:29:43 -0300

    semantic_tokens: Handle multiple responses

--- src/language_features/semantic_tokens.rs
+++ src/language_features/semantic_tokens.rs
@@ -1,7 +1,7 @@
 use crate::capabilities::{attempt_server_capability, CAPABILITY_SEMANTIC_TOKENS};
 use crate::context::{Context, RequestParams};
 use crate::position::lsp_range_to_kakoune;
-use crate::types::EditorMeta;
+use crate::types::{EditorMeta, ServerName};
 use crate::util::editor_quote;
 use indoc::formatdoc;
 use lsp_types::request::SemanticTokensFullRequest;
@@ -13,33 +13,56 @@ use lsp_types::{
 use url::Url;
 
 pub fn tokens_request(meta: EditorMeta, ctx: &mut Context) {
-    let entry = ctx.language_servers.first_key_value().unwrap();
-    if meta.fifo.is_none() && !attempt_server_capability(entry, &meta, CAPABILITY_SEMANTIC_TOKENS) {
+    let eligible_servers: Vec<_> = ctx
+        .language_servers
+        .iter()
+        .filter(|srv| attempt_server_capability(*srv, &meta, CAPABILITY_SEMANTIC_TOKENS))
+        .collect();
+    if meta.fifo.is_none() && eligible_servers.is_empty() {
         return;
     }
 
-    let req_params = SemanticTokensParams {
-        partial_result_params: Default::default(),
-        text_document: TextDocumentIdentifier {
-            uri: Url::from_file_path(&meta.buffile).unwrap(),
-        },
-        work_done_progress_params: Default::default(),
-    };
+    let (first_server, _) = eligible_servers.first().unwrap();
+    let first_server = first_server.to_string();
+
+    let req_params = eligible_servers
+        .into_iter()
+        .map(|(server_name, _)| {
+            (
+                server_name.clone(),
+                vec![SemanticTokensParams {
+                    partial_result_params: Default::default(),
+                    text_document: TextDocumentIdentifier {
+                        uri: Url::from_file_path(&meta.buffile).unwrap(),
+                    },
+                    work_done_progress_params: Default::default(),
+                }],
+            )
+        })
+        .collect();
     ctx.call::<SemanticTokensFullRequest, _>(
         meta,
-        RequestParams::All(vec![req_params]),
-        move |ctx, meta, mut response| {
-            if let Some((_, response)) = response.pop() {
-                if let Some(response) = response {
-                    tokens_response(meta, response, ctx);
-                }
+        RequestParams::Each(req_params),
+        move |ctx, meta, results| {
+            let (server_name, response) = match results.into_iter().find(|(_, v)| v.is_some()) {
+                Some(result) => result,
+                None => (first_server, None),
+            };
+
+            if let Some(response) = response {
+                tokens_response(meta, (server_name, response), ctx);
             }
         },
     );
 }
 
-pub fn tokens_response(meta: EditorMeta, tokens: SemanticTokensResult, ctx: &mut Context) {
-    let (_, server) = ctx.language_servers.first_key_value().unwrap();
+pub fn tokens_response(
+    meta: EditorMeta,
+    response: (ServerName, SemanticTokensResult),
+    ctx: &mut Context,
+) {
+    let (server_name, tokens) = response;
+    let server = &ctx.language_servers[&server_name];
     let legend = match server
         .capabilities
         .as_ref()
